51eba94cbcefa8c7cb691dee9200c9509a462793
Graham Reeves <graham@grahamreeves.com>
2018-07-26 12:34:40 +0100

    Added enough wrappers (browser proxy) to load tensorflow & posenet
    Former-commit-id: a3c8de83687223eca698bd10f1823024affbebbb

--- Data_Posenet/Bootup.js
+++ Data_Posenet/Bootup.js
@@ -1,3 +1,154 @@
+function OpenglExtension_LoseContext()
+{
+	this.loseContext = function()
+	{
+		Debug("Do loseContext");
+	}
+}
+
+function OpenglExtension_EXTColorBufferFloat()
+{
+	
+}
+
+function OpenglDataBuffer(Name)
+{
+	this.Name = Name;
+}
+
+function OpenglFrameBuffer(Name)
+{
+	this.Name = Name;
+}
+
+function FakeOpenglContext(ContextType)
+{
+	Debug("FakeOpenglContext(" + ContextType + ")");
+
+	this.DataBufferCounter = 0;
+	this.FrameBufferCounter = 0;
+	
+	this.getExtension = function(ExtensionName)
+	{
+		if ( ExtensionName == "WEBGL_lose_context" )
+			return new OpenglExtension_LoseContext();
+		if ( ExtensionName == "EXT_color_buffer_float" )
+			return new OpenglExtension_EXTColorBufferFloat();
+		
+		//WEBGL_get_buffer_sub_data_async
+		return null;
+	}
+	//WebGLRenderingContext.getSupportedExtensions.
+	
+	this.disable = function(GlStateEnum)	{	Debug("gldisable(" + GlStateEnum +")");	}
+	this.enable = function(GlStateEnum)		{	Debug("glenable(" + GlStateEnum +")");	}
+	this.cullFace = function(CullFaceEnum)	{	Debug("cullFace(" + CullFaceEnum +")");	}
+
+	this.createBuffer = function()
+	{
+		this.DataBufferCounter++;
+		let NewBuffer = new OpenglDataBuffer(this.DataBufferCounter);
+		return NewBuffer;
+	}
+	
+	this.bindBuffer = function(BufferBinding,Buffer)
+	{
+		Debug("BindBuffer( " + BufferBinding + ", " + Buffer.Name + ")" );
+	}
+	
+	this.bufferData = function(BufferBinding,Data,Mode)
+	{
+		Debug("bufferData( " + BufferBinding + ", " + Data + ", " + Mode + " )" );
+	}
+	
+	this.createFramebuffer = function()
+	{
+		this.FrameBufferCounter++;
+		let NewBuffer = new OpenglFrameBuffer(this.FrameBufferCounter);
+		return NewBuffer;
+		
+	}
+}
+
+/*
+ unction bindVertexBufferToProgramAttribute(e, t, r, n, a, i, o) {
+ var s = e.getAttribLocation(t, r);
+ return -1 !== s && (callAndCheck(e, function() {
+ return e.bindBuffer(e.ARRAY_BUFFER, n)
+ }),
+ callAndCheck(e, function() {
+ return e.vertexAttribPointer(s, a, e.FLOAT, !1, i, o)
+ }),
+ callAndCheck(e, function() {
+ return e.enableVertexAttribArray(s)
+ }),
+ !0)*/
+
+function FakeCanvas()
+{
+	this.Context = null;
+	
+	let This = this;
+	this.getContext = function(ContextType)
+	{
+		if ( This.Context == null )
+		{
+			This.Context = new FakeOpenglContext(ContextType);
+		}
+		return This.Context;
+	}
+	
+}
+
+function FakeScreen()
+{
+	this.width = 999;
+	this.height = 888;
+}
+
+function FakeWindow()
+{
+	this.screen = new FakeScreen();
+}
+
+function FakeDocument()
+{
+	this.createElement = function(Type)
+	{
+		if ( Type == "canvas" )
+			return new FakeCanvas();
+		
+		throw "Need to create a fake " + Type;
+	}
+}
+
+function FakeConsole()
+{
+	this.log = Debug;
+	this.warn = Debug;
+}
+
+
+function createVertexShader$1(e) {
+	return createVertexShader(e, "\n    precision highp float;\n    attribute vec3 clipSpacePos;\n    attribute vec2 uv;\n    varying vec2 resultUV;\n\n    void main() {\n      gl_Position = vec4(clipSpacePos, 1);\n      resultUV = uv;\n    }")
+}
+function createVertexBuffer(e) {
+	return createStaticVertexBuffer(e, new Float32Array([-1, 1, 0, 0, 1, -1, -1, 0, 0, 0, 1, 1, 0, 1, 1, 1, -1, 0, 1, 0]))
+}
+function createIndexBuffer(e) {
+	return createStaticIndexBuffer(e, new Uint16Array([0, 1, 2, 2, 1, 3]))
+}
+
+
+
+
+//	gr: window wrapper to emulate browser for tensor flow
+var window = new FakeWindow();
+var console = new FakeConsole();
+var document = new FakeDocument();
+
+
+
 //	gr: include is not a generic thing (or a wrapper yet) so we can change
 //	LoadFileAsString to a file-handle to detect file changes to auto reload things
 function include(Filename)
@@ -6,6 +157,7 @@ function include(Filename)
 	CompileAndRun( Source );
 }
 include("Data_Posenet/tfjs.0.11.7.js");
+include("Data_Posenet/posenet.0.1.2.js");
 //include("Data_Posenet/Hello.js");
 
 let VertShaderSource = `
