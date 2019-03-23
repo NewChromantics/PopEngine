ef1ab763d5e84fcb52bd5b03856e2f59202325f9
Graham Reeves <graham@grahamreeves.com>
2019-03-23 14:07:25 +0000

    Poph264 dll 5b724a
    Former-commit-id: 60cc8a7b7ad89a49ae9a5e4035a5b00359377ac9

---
+++ src/Libs/PopH264/PopH264.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <stdint.h>
+
+
+#if !defined(__export)
+
+#if defined(_MSC_VER) && !defined(TARGET_PS4)
+	#define __export			extern "C" __declspec(dllexport)
+#else
+	#define __export			extern "C"
+#endif
+
+#endif
+
+/*
+#if defined(TARGET_WINDOWS)
+#include <SDKDDKVer.h>
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+*/
+
+
+__export int32_t			CreateInstance();
+__export void				DestroyInstance(int32_t Instance);
+
+//	todo: document values with a function that outputs labels!
+__export void				GetMeta(int32_t Instance,int32_t* MetaValues,int32_t MetaValuesCount);
+
+//	expecting one frame per packet, split up by highlevel code (mp4 demuxer etc)
+__export int32_t			PushData(int32_t Instance,uint8_t* Data,int32_t DataSize,int32_t FrameNumber);
+
+//	returns frame number
+__export int32_t			PopFrame(int32_t Instance,uint8_t* Plane0,int32_t Plane0Size,uint8_t* Plane1,int32_t Plane1Size,uint8_t* Plane2,int32_t Plane2Size);
+
