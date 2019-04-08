6a2ba7717eb92e1cd3fd1f6b9753784ad0ef6681
Graham Reeves <graham@grahamreeves.com>
2019-04-08 13:45:03 +0100

    Deleted unneeded DLL binding code
    Former-commit-id: 21e798be04560d3b0d83838a505121fbe8b71306

--- src/JsCoreDll.cpp
+++ src/JsCoreDll.cpp
@@ -10,85 +10,6 @@ namespace JsCore
 	std::shared_ptr<Soy::TRuntimeLibrary> JavascriptCoreDll;
 }
 
-	/*
-
-typedef decltype(JSEvaluateScript) FUNCTIONTYPE;
-
-template<typename FUNCTION>
-class TAutoLinkedFunction
-{
-public:
-	typedef FUNCTION TFUNCTION;
-	static TFUNCTION* Static;
-};
-TAutoLinkedFunction<decltype(JSEvaluateScript)>::TFUNCTION JSEvaluateScript;
-decltype(JSEvaluateScript) JSEvaluateScript = (decltype(JSEvaluateScript))&Blah;
-//extern "C" decltype(JSEvaluateScript) JSEvaluateScript;
-*/
-
-BufferArray<std::function<void()>, 100> LoadFuncs;
-void AddToLoadList(std::function<void()> Load)
-{
-	LoadFuncs.PushBack(Load);
-}
-/*
-
-template <const size_t _UniqueId, typename _Res, typename... _ArgTypes>
-struct fun_ptr_helper
-{
-public:
-	typedef std::function<_Res(_ArgTypes...)> function_type;
-
-	static void bind(function_type&& f)
-	{ instance().fn_.swap(f); }
-
-	static void bind(const function_type& f)
-	{ instance().fn_=f; }
-
-	static _Res invoke(_ArgTypes... args)
-	{ return instance().fn_(args...); }
-
-	typedef decltype(&fun_ptr_helper::invoke) pointer_type;
-	static pointer_type ptr()
-	{ return &invoke; }
-
-private:
-	static fun_ptr_helper& instance()
-	{
-		static fun_ptr_helper inst_;
-		return inst_;
-	}
-
-	fun_ptr_helper() {}
-
-	function_type fn_;
-};
-
-template <const size_t _UniqueId, typename _Res, typename... _ArgTypes>
-typename fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::pointer_type
-get_fn_ptr(const std::function<_Res(_ArgTypes...)>& f)
-{
-	fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::bind(f);
-	return fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::ptr();
-}
-
-template<typename T>
-std::function<typename std::enable_if<std::is_function<T>::value, T>::type>
-make_function(T *t)
-{
-	return {t};
-}
-
-
-
-template <const size_t _UniqueId, typename _Res, typename... _ArgTypes>
-typename fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::pointer_type
-get_fn_ptr(const std::function<_Res(_ArgTypes...)>& f)
-{
-	fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::bind(f);
-	return fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::ptr();
-}
-*/
 
 
 template<const char* SymbolName,typename RESULTTYPE, typename... ARGTYPES>
@@ -153,26 +74,6 @@ void LoadFunction(Soy::TRuntimeLibrary& Library)
 	Library.SetFunction(Function, FUNCTIONNAME);
 }
 
-/*
-template<typename FUNCTION>
-FUNCTION* GetFunction(const char* Name)
-{
-	static std::function<FUNCTION> Cache;
-	auto Loader = [&]()
-	{
-		//Cache = []()
-		{
-			std::Debug << "Hello " << Name << std::endl;
-		}
-	};
-	AddToLoadList(Loader);
-	return Cache;
-}
-auto* x = get_fn_ptr<0>(make_function(JSEvaluateScript));
-*/
-
-
-//template<> inline void LoadFunction<decltype(FUNCTIONNAME)>(Soy::TRuntimeLibrary& Library)	{	LoadFunction<FUNCTIONNAME ## _FuncName,decltype(FUNCTIONNAME)>( Library );	}	\
 
 template<void* FUNCTIONPTR>
 void LoadFunction(Soy::TRuntimeLibrary& Library)
@@ -262,23 +163,6 @@ DEFINE_JS_BRIDGE(JSPropertyNameArrayGetCount);
 DEFINE_JS_BRIDGE(JSPropertyNameArrayGetNameAtIndex);
 DEFINE_JS_BRIDGE(JSPropertyNameAccumulatorAddName);
 
-
-
-
-
-
-
-
-
-//auto JSEvaluateScript_FuncName = "JSEvaluateScript";
-
-//extern "C" auto* __imp_JSEvaluateScript = &JSEvaluateScript;
-/*
-extern "C" FUNCTIONTYPE JSEvaluateScript(...)
-{
-	//= GetFunction<decltype(JSEvaluateScript)>();
-}
-*/
 
 #define LOAD_FUNCTION(FUNCTIONNAME)		LoadFunction<FUNCTIONNAME ## _FuncName,decltype(FUNCTIONNAME)>(Library)
 
