2d97df195685d24f6362a52c1b84ee533008b304
Graham Reeves <graham@grahamreeves.com>
2019-11-15 16:38:07 +0000

    Renamed PopTrack project stuff to PopEngine in xcode
    Former-commit-id: 41edca20f5c798e719b9fd1823d3531501be67ac

---
+++ PopEngine/PopEngine.h
@@ -0,0 +1,19 @@
+//
+//  PopEngine.h
+//  PopEngine
+//
+//  Created by graham on 15/11/2019.
+//  Copyright © 2019 NewChromantics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopEngine.
+FOUNDATION_EXPORT double PopEngineVersionNumber;
+
+//! Project version string for PopEngine.
+FOUNDATION_EXPORT const unsigned char PopEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopEngine/PublicHeader.h>
+
+
