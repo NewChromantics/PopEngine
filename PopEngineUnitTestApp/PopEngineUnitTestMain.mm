730b1c6cb7a1c2789ce8c7832f2b2a39c85e914b
Graham Reeves <graham@grahamreeves.com>
2019-11-19 14:24:20 +0000

    Added unit test app which loads the framework as a framework and runs
    Former-commit-id: ab1519eccab794368fedda7db87df73b469fca23

--- PopEngineUnitTestApp/PopEngineUnitTestMain.mm
+++ PopEngineUnitTestApp/PopEngineUnitTestMain.mm
@@ -3,7 +3,17 @@
 
 int main(int argc, const char * argv[])
 {
-	std::cout << "Hello, World!";
+	//	if first arg is a path, then to make debugging easier in xcode,
+	//	lets load that project instead of unit tests
+	std::string ProjectPath = "UnitTest";
+	
+	if ( argc > 1 )
+	{
+		//if ( Platform::IsFullPath(argv[0] )
+		ProjectPath = argv[1];
+	}
+	
+	std::cout << "Running project " << ProjectPath;
 	//	Unit test files should be under resources in build
-	return PopEngine("UnitTest");
+	return PopEngine( ProjectPath.c_str() );
 }
