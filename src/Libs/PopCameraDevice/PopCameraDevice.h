df2762510e91a8163090c64305bf7ef8e729c1c3
Graham Reeves <graham@grahamreeves.com>
2020-03-04 17:20:00 +0000

    Update of PopCameraDevice meta files
    Former-commit-id: 04076897df6b6a4978cca7f3a5bd9d42e3197758

--- src/Libs/PopCameraDevice/PopCameraDevice.h
+++ src/Libs/PopCameraDevice/PopCameraDevice.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//	if you're using this header to link to the DLL, you'll probbaly need the lib :)
+//	if you're using this header to link to the DLL, you'll probbaly need the lib
 //#pragma comment(lib, "PopCameraDevice.lib")
 
 #include <stdint.h>
@@ -8,7 +8,7 @@
 
 #if !defined(__export)
 
-#if defined(_MSC_VER) && !defined(TARGET_PS4)
+#if defined(_MSC_VER) && !defined(TARGET_PS4)	//	_MSC_VER = visual studio
 #define __export			extern "C" __declspec(dllexport)
 #else
 #define __export			extern "C"
@@ -16,26 +16,36 @@
 
 #endif
 
+//	function pointer type for new frame callback
+typedef void PopCameraDevice_OnNewFrame(void* Meta);
 
-//	forward declare this c++ class. May need to export the class...
-#if defined(__cplusplus)
-namespace PopCameraDevice
-{
-	class TDevice;
-}
-#define POPCAMERADEVICE_EXPORTCLASS	PopCameraDevice::TDevice
-#else
-#define POPCAMERADEVICE_EXPORTCLASS	void
-#endif
+//	enum every availible device and their availible formats as Json
+__export void				PopCameraDevice_EnumCameraDevicesJson(char* StringBuffer,int32_t StringBufferLength);
 
-//	for C++ interfaces, to give access to known types and callbacks
-//	todo: proper shared_ptr sharing, dllexport class etc. this is essentially unsafe, but caller can manage this between CreateInstance and DestroyInstance
-__export POPCAMERADEVICE_EXPORTCLASS*		PopCameraDevice_GetDevicePtr(int32_t Instance);
+//	create a new device. Format is optional. Returns instance ID (0 on error)
+__export int32_t			PopCameraDevice_CreateCameraDeviceWithFormat(const char* Name,const char* Format, char* ErrorBuffer, int32_t ErrorBufferLength);
 
-__export void				PopCameraDevice_EnumCameraDevices(char* StringBuffer,int32_t StringBufferLength);
-__export int32_t			PopCameraDevice_CreateCameraDevice(const char* Name);
 __export void				PopCameraDevice_FreeCameraDevice(int32_t Instance);
-__export void				PopCameraDevice_GetMeta(int32_t Instance,int32_t* MetaValues,int32_t MetaValuesCount);
-__export int32_t			PopCameraDevice_PopFrame(int32_t Instance, uint8_t* Plane0, int32_t Plane0Size, uint8_t* Plane1, int32_t Plane1Size, uint8_t* Plane2, int32_t Plane2Size);
-__export int32_t			PopCameraDevice_PopFrameAndMeta(int32_t Instance, uint8_t* Plane0, int32_t Plane0Size, uint8_t* Plane1, int32_t Plane1Size, uint8_t* Plane2, int32_t Plane2Size, char* MetaBuffer, int32_t MetaBufferLength);
+
+//	register a callback function when a new frame is ready. This is expected to exist until released
+__export void				PopCameraDevice_AddOnNewFrameCallback(int32_t Instance, PopCameraDevice_OnNewFrame* Callback, void* Meta);
+
+//	returns -1 if no new frame
+//	Fills in meta with JSON about frame
+//	Next frame is not deleted. 
+//	Meta will list other frames buffered up, so to skip to latest frame, Pop without buffers
+__export int32_t			PopCameraDevice_PeekNextFrame(int32_t Instance,char* MetaJsonBuffer,int32_t MetaJsonBufferSize);
+
+//	returns -1 if no new frame
+//	Deletes frame.
+__export int32_t			PopCameraDevice_PopNextFrame(int32_t Instance, char* MetaJsonBuffer, int32_t MetaJsonBufferSize, uint8_t* Plane0, int32_t Plane0Size, uint8_t* Plane1, int32_t Plane1Size, uint8_t* Plane2, int32_t Plane2Size);
+
+//	returns	version integer as A.BBB.CCCCCC (major, minor, patch. Divide by 10's to split)
+__export int32_t			PopCameraDevice_GetVersion();
+
+//	shouldn't need this, but cleans up explicitly
+__export void				PopCameraDevice_Cleanup();
+
+//	for internal building really
+__export void				PopCameraDevice_UnitTests();
 
