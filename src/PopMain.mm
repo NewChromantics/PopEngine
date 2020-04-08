1cd18a26cf7206d02ed2814396196afc1a4b2fac
Graham Reeves <graham@grahamreeves.com>
2020-04-08 22:03:30 +0100

    Now running on ios!
    Former-commit-id: 43db771c060fc1231f6587e898f0f48545783594

--- src/PopMain.mm
+++ src/PopMain.mm
@@ -32,7 +32,13 @@ int Soy::Platform::BundleAppMain()
 //	UIKIT_EXTERN int UIApplicationMain(int argc, char * _Nullable argv[_Nonnull], NSString * _Nullable principalClassName, NSString * _Nullable delegateClassName);
 	//const char *[1]' to 'char * _Nullable *
 	char* argv[] = {"FakeExe"};
-	return UIApplicationMain(argc, argv, nullptr, nullptr );
+
+	//	when using storyboards, we need to set an app delegate class;
+	//	error:
+	//	There is no app delegate set. An app delegate class must be specified to use a main storyboard file.
+	//	https://stackoverflow.com/a/17278607/355753
+	auto AppDelegateClass = @"AppDelegate";
+	return UIApplicationMain(argc, argv, nullptr, AppDelegateClass );
 #else
 	const char* argv[] = {"FakeExe"};
 	return NSApplicationMain(argc, argv);
@@ -71,11 +77,15 @@ void PopMainThread::TriggerIteration()
 #if defined(TARGET_OSX)
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 #elif defined(TARGET_IOS)
-@interface AppDelegate : NSObject <UIApplicationDelegate>
+//@interface AppDelegate : NSObject <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
 #endif
 
 //@property (assign) IBOutlet NSWindow *window;
 //@property (assign) IBOutlet SKView *skView;
+
+
+@property (strong, nonatomic) UIWindow *window;
 
 @end
 
@@ -130,5 +140,41 @@ void PopMainThread::TriggerIteration()
 	return YES;
 }
 #endif
+
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	// Override point for customization after application launch.
+	PopMain();
+	return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+	// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+	// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
 
 @end
