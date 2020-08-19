364bb82a3cac798c965a88b5f525bf292ce2c95e
Graham Reeves <graham@grahamreeves.com>
2020-08-19 11:44:11 +0100

    Pop.Panopoly.DepthToYuv now requires the output yuv format

--- src/TApiPanopoly.cpp
+++ src/TApiPanopoly.cpp
@@ -66,29 +66,39 @@ void ApiPanopoly::Bind(Bind::TContext& Context)
 #include "PopDepthToYuv/Depth16ToYuv.c"
 
 
-void DepthToYuv(SoyPixelsImpl& DepthPixels,SoyPixelsImpl& YuvPixels,EncodeParams_t EncodeParams)
+void DepthToYuv(SoyPixelsImpl& DepthPixels,SoyPixelsImpl& YuvPixels,EncodeParams_t EncodeParams,SoyPixelsFormat::Type OutputYuvFormat)
 {
 	Soy::TScopeTimerPrint Timer(__PRETTY_FUNCTION__,20);
 	auto Width = DepthPixels.GetWidth();
 	auto Height = DepthPixels.GetHeight();
-	YuvPixels.Init(SoyPixelsMeta( Width, Height, SoyPixelsFormat::Yuv_8_8_8 ));
+	YuvPixels.Init(SoyPixelsMeta( Width, Height, OutputYuvFormat ));
 	BufferArray<std::shared_ptr<SoyPixelsImpl>, 3> YuvPlanes;
 	YuvPixels.SplitPlanes(GetArrayBridge(YuvPlanes));
 
 	struct Meta_t
 	{
-		uint8_t* LumaPixels;
-		uint8_t* ChromaUPixels;
-		uint8_t* ChromaVPixels;
-		size_t Width;
+		uint8_t* LumaPixels = nullptr;
+		uint8_t* ChromaUPixels = nullptr;
+		uint8_t* ChromaVPixels = nullptr;
+		uint16_t* ChromaUVPixels = nullptr;
+		size_t Width = 0;
 	};
 	Meta_t Meta;
 	Meta.LumaPixels = YuvPlanes[0]->GetPixelsArray().GetArray();
-	Meta.ChromaUPixels = YuvPlanes[1]->GetPixelsArray().GetArray();
-	Meta.ChromaVPixels = YuvPlanes[2]->GetPixelsArray().GetArray();
+	if (YuvPlanes.GetSize() == 3)
+	{
+		Meta.ChromaUPixels = YuvPlanes[1]->GetPixelsArray().GetArray();
+		Meta.ChromaVPixels = YuvPlanes[2]->GetPixelsArray().GetArray();
+	}
+	else if (YuvPlanes.GetSize() == 2)
+	{
+		auto* uv8 = YuvPlanes[1]->GetPixelsArray().GetArray();
+		Meta.ChromaUVPixels = reinterpret_cast<uint16_t*>(uv8);
+	}
 	Meta.Width = Width;
+
 	
-	auto WriteYuv = [](uint32_t x, uint32_t y, uint8_t Luma, uint8_t ChromaU, uint8_t ChromaV, void* pMeta)
+	auto WriteYuv_8_8_8 = [](uint32_t x, uint32_t y, uint8_t Luma, uint8_t ChromaU, uint8_t ChromaV, void* pMeta)
 	{
 		auto& Meta = *reinterpret_cast<Meta_t*>(pMeta);
 		auto cx = x / 2;
@@ -103,6 +113,20 @@ void DepthToYuv(SoyPixelsImpl& DepthPixels,SoyPixelsImpl& YuvPixels,EncodeParams
 		//ChromaUVPlane.SetPixel(cx, cy, 0, ChromaU);
 		//ChromaUVPlane.SetPixel(cx, cy, 1, ChromaV);
 	};
+
+	auto WriteYuv_8_88 = [](uint32_t x, uint32_t y, uint8_t Luma, uint8_t ChromaU, uint8_t ChromaV, void* pMeta)
+	{
+		auto& Meta = *reinterpret_cast<Meta_t*>(pMeta);
+		auto cx = x / 2;
+		auto cy = y / 2;
+		auto cw = Meta.Width / 2;
+		auto i = x + (y*Meta.Width);
+		auto ci = cx + (cy*cw);
+		Meta.LumaPixels[i] = Luma;
+		auto* ChromaUv8 = reinterpret_cast<uint8_t*>(&Meta.ChromaUVPixels[ci]);
+		ChromaUv8[0] = ChromaU;
+		ChromaUv8[1] = ChromaV;
+	};
 	
 	auto OnErrorCAPI = [](const char* Error,void* This)
 	{
@@ -110,22 +134,34 @@ void DepthToYuv(SoyPixelsImpl& DepthPixels,SoyPixelsImpl& YuvPixels,EncodeParams
 		std::Debug << "Depth16ToYuv error; " << Error << std::endl;
 	};
 	
-	if ( DepthPixels.GetFormat() == SoyPixelsFormat::Depth16mm )
+	if (DepthPixels.GetFormat() == SoyPixelsFormat::Depth16mm && YuvPixels.GetFormat() == SoyPixelsFormat::Yuv_8_8_8)
 	{
 		auto* Depth16 = reinterpret_cast<uint16_t*>(DepthPixels.GetPixelsArray().GetArray());
-		Depth16ToYuv(Depth16, Width, Height, EncodeParams, WriteYuv, OnErrorCAPI, &Meta );
+		Depth16ToYuv(Depth16, Width, Height, EncodeParams, WriteYuv_8_8_8, OnErrorCAPI, &Meta);
 		return;
 	}
-	else if ( DepthPixels.GetFormat() == SoyPixelsFormat::DepthFloatMetres )
+	else if (DepthPixels.GetFormat() == SoyPixelsFormat::DepthFloatMetres && YuvPixels.GetFormat() == SoyPixelsFormat::Yuv_8_8_8)
 	{
 		auto* Depthf = reinterpret_cast<float*>(DepthPixels.GetPixelsArray().GetArray());
-		DepthfToYuv(Depthf, Width, Height, EncodeParams, WriteYuv, OnErrorCAPI, &Meta );
+		DepthfToYuv(Depthf, Width, Height, EncodeParams, WriteYuv_8_8_8, OnErrorCAPI, &Meta);
+		return;
+	}
+	else if (DepthPixels.GetFormat() == SoyPixelsFormat::Depth16mm && YuvPixels.GetFormat() == SoyPixelsFormat::Yuv_8_88)
+	{
+		auto* Depth16 = reinterpret_cast<uint16_t*>(DepthPixels.GetPixelsArray().GetArray());
+		Depth16ToYuv(Depth16, Width, Height, EncodeParams, WriteYuv_8_88, OnErrorCAPI, &Meta);
+		return;
+	}
+	else if (DepthPixels.GetFormat() == SoyPixelsFormat::DepthFloatMetres && YuvPixels.GetFormat() == SoyPixelsFormat::Yuv_8_88)
+	{
+		auto* Depthf = reinterpret_cast<float*>(DepthPixels.GetPixelsArray().GetArray());
+		DepthfToYuv(Depthf, Width, Height, EncodeParams, WriteYuv_8_88, OnErrorCAPI, &Meta);
 		return;
 	}
 
 	
 	std::stringstream Error;
-	Error << "Unsupported pixel format " << DepthPixels.GetFormat() << " for conversion to depth yuv";
+	Error << "Unsupported pixel formats from:" << DepthPixels.GetFormat() << " to " << YuvPixels.GetFormat() << " for conversion to depth yuv";
 	throw Soy::AssertException(Error);
 }
 
@@ -143,6 +179,12 @@ void ApiPanopoly::DepthToYuvAsync(Bind::TCallback& Params)
 	EncodeParams.DepthMax = Params.GetArgumentInt(2);
 	EncodeParams.ChromaRangeCount = Params.GetArgumentInt(3);
 	EncodeParams.PingPongLuma = Params.GetArgumentBool(4);
+
+	//	gr: make a GetArgumentStringEnum() func for nice auto errors
+	if (!Params.IsArgumentString(5))
+		throw Soy::AssertException("Now expecting YUV output format param");
+	auto EncodeYuvFormatString = Params.GetArgumentString(5);
+	SoyPixelsFormat::Type EncodeYuvFormat = SoyPixelsFormat::ToType(EncodeYuvFormatString);
 	
 	//	create a promise in a promise map so we don't accidentally delete it in the wrong thread
 	auto& PanopolyContext = Panopoly::GetContext();
@@ -159,7 +201,7 @@ void ApiPanopoly::DepthToYuvAsync(Bind::TCallback& Params)
 		{
 			pDepthPixels->Flip();
 			std::shared_ptr<SoyPixelsImpl> pYuvPixels( new SoyPixels );
-			DepthToYuv(*pDepthPixels,*pYuvPixels,EncodeParams);
+			DepthToYuv(*pDepthPixels,*pYuvPixels,EncodeParams, EncodeYuvFormat);
 			
 			auto ResolvePromise = [=](Bind::TLocalContext& LocalContext,Bind::TPromise& Promise)
 			{
