d25d623a3db73c4be362ef041119068c19206e08
thomasSDK <tsdkelly@protonmail.com>
2020-09-02 17:52:47 +0100

    add initial SoyGuiLinux class

---
+++ src/SoyGuiLinux.cpp
@@ -0,0 +1,33 @@
+#include "SoyGui.h"
+#include "esUtil.h"
+
+class Platform::TWindow : public SoyWindow
+{
+public:
+	TWindow(const std::string& Name);
+	
+	virtual Soy::Rectx<int32_t>		GetScreenRect() override;
+
+	ESContext											esContext;
+};
+
+Platform::TWindow::TWindow(const std::string& Name)
+{
+	esInitContext(&esContext);
+
+	esCreateWindow( &esContext, Name, 320, 240, ES_WINDOW_ALPHA);
+}
+
+std::shared_ptr<SoyWindow> Platform::CreateWindow(const std::string& Name,Soy::Rectx<int32_t>& Rect,bool Resizable)
+{
+	std::shared_ptr<SoyWindow> Window;
+
+	Window.reset( new Platform::TWindow(Name) );
+
+	return Window;
+}
+
+Soy::Rectx<int32_t> Platform::TWindow::GetScreenRect()
+{
+	Soy_AssertTodo();
+}
