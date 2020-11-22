73a4c0a83b1b53df986ab25aba5f9f2f8237b78f
Graham Reeves <graham@grahamreeves.com>
2020-11-22 18:56:15 +0000

    Added a test swiftUI file to test building

---
+++ Source_Ios/TestGui.swift
@@ -0,0 +1,22 @@
+//
+//  TestGui.swift
+//  PopEngineTestApp_Ios
+//
+//  Created by Graham Reeves on 22/11/2020.
+//  Copyright © 2020 NewChromantics. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestGui: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct TestGui_Previews: PreviewProvider {
+    static var previews: some View {
+        TestGui()
+    }
+}
